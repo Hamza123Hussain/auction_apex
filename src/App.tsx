5f946349b863a9c966f662494dc2915fe6e2040d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-04 03:30:55 +0500

    routes established

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Login from './components/Auth/Login'
+import Signup from './components/Auth/SignUp'
+import ResetPassword from './components/Auth/ResetPassword'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow px-4 my-10 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+        <Routes>
+          <Route path="/" />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/ResetPass" element={<ResetPassword />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

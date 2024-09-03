8e52123f4952351c1737553b4b9bab7ffee85eb5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-04 03:29:14 +0500

    dummy context has been setup

---
+++ src/utils/Context.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { InputValues, UserData } from './SignupInterface'
+
+export const UserContext = createContext<any>(null)
+
+const ContextProvider = ({ children }: { children: ReactNode }) => {
+  // States
+  const [inputVal, setInputVal] = useState<InputValues>({
+    email: '',
+    password: '',
+    Name: '',
+    Image: null,
+  })
+
+  const [loading, setLoading] = useState(true) // Start with loading true
+
+  const [userData, setUserData] = useState<UserData>(() => {
+    try {
+      const storedData = localStorage.getItem('userData')
+      return storedData ? JSON.parse(storedData) : {} // Initialize with empty object
+    } catch (error) {
+      console.error('Failed to parse userData from localStorage:', error)
+      return {} // Fallback to empty object
+    }
+  })
+
+  // Effect for saving userData to local storage
+  useEffect(() => {
+    try {
+      localStorage.setItem('userData', JSON.stringify(userData))
+    } catch (error) {
+      console.error('Failed to save userData to localStorage:', error)
+    }
+  }, [userData])
+
+  // Effect for loading state and timer for demonstration purposes
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1000) // Simulated delay
+    return () => clearTimeout(timer) // Cleanup on unmount
+  }, [])
+
+  return (
+    <UserContext.Provider
+      value={{
+        userData,
+        setUserData,
+        loading,
+        setLoading,
+        inputVal,
+        setInputVal,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export default ContextProvider

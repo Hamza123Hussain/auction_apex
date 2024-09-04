bf952f5b1286843683902aa3dd942c19f5b55a86
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 04:23:37 +0500

    array for input fields to make the main component clean

---
+++ src/components/Auction/InputFieldArray.ts
@@ -0,0 +1,20 @@
+export const inputFields = [
+  {
+    id: 'product',
+    label: 'Product ID',
+    type: 'text',
+    required: true,
+  },
+  {
+    id: 'startDate',
+    label: 'Start Date',
+    type: 'datetime-local',
+    required: true,
+  },
+  {
+    id: 'endDate',
+    label: 'End Date',
+    type: 'datetime-local',
+    required: true,
+  },
+]

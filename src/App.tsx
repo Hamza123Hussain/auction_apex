672a82bfd7e1ea93c3ce5364e1bec472669b36fd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-04 07:44:13 +0500

    not letting user go to auth paths when signed in

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -9,17 +7,24 @@ import Signup from './components/Auth/SignUp'
 import ResetPassword from './components/Auth/ResetPassword'
 import ProductAddForm from './components/Product/CreateProduct'
 import ProductPage from './components/Product/ProductPage'
+import { UserContext } from './utils/Context'
 
-function App() {
+const App = () => {
+  const { userData } = useContext(UserContext)
+
+  // Determine if the user is authenticated
+  const renderRoute = (Component: React.ElementType) =>
+    userData._id ? <ProductPage /> : <Component />
+
   return (
     <div className="App flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow justify-center items-center p-4   my-10 ">
+      <main className="flex-grow p-4 my-10">
         <Routes>
           <Route path="/" element={<ProductPage />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/register" element={<Signup />} />
-          <Route path="/ResetPass" element={<ResetPassword />} />
+          <Route path="/Login" element={renderRoute(Login)} />
+          <Route path="/register" element={renderRoute(Signup)} />
+          <Route path="/ResetPass" element={renderRoute(ResetPassword)} />
           <Route path="/AddProduct" element={<ProductAddForm />} />
         </Routes>
       </main>

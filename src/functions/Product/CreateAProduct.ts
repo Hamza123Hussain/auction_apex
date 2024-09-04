7c2bbb145f9e20a8c64894c9f0439d8a83585b6b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-04 07:42:04 +0500

    divided into mutliple components and styled it

--- src/functions/Product/CreateAProduct.ts
+++ src/functions/Product/CreateAProduct.ts
@@ -1,40 +1,41 @@
-import axios from 'axios'
-
-export const createProduct = async (productData: any) => {
+export const createProduct = async (
+  productData: any,
+  imageFile: File | null
+) => {
   const formData = new FormData()
 
   // Append product data to the form
   formData.append('productName', productData.productName)
   formData.append('price', productData.price)
-  //   formData.append('auctionEndDate', productData.auctionEndDate)
+  formData.append('auctionEndDate', productData.auctionEndDate)
   formData.append('status', productData.status)
   formData.append('sellerId', `yOmtiVVUX8ToaFDOnSNzHQXWOok1`)
 
-  if (productData.image) {
-    formData.append('image', productData.image)
+  // Append image file if available
+  if (imageFile) {
+    formData.append('image', imageFile)
   }
 
   try {
-    const response = await axios.post(
+    const response = await fetch(
       'http://localhost:5000/API/Product/AddProduct',
-      formData, // <-- Pass formData directly here
       {
-        headers: {
-          'Content-Type': 'multipart/form-data', // Set the correct content type for form data
-        },
+        method: 'POST',
+        body: formData,
       }
     )
 
-    if (response.status === 201) {
-      console.log('Product created successfully:', response.data)
-      return response.data
-    }
-  } catch (error: any) {
-    console.error('Error:', error)
-    if (error.response && error.response.data) {
-      alert(`Error: ${error.response.data.error}`)
+    if (response.ok) {
+      const data = await response.json()
+      console.log('Product created successfully:', data)
+      return data
     } else {
-      alert('An unexpected error occurred.')
+      const errorData = await response.json()
+      console.error('Error creating product:', errorData)
+      alert(`Error: ${errorData.error}`)
     }
+  } catch (error) {
+    console.error('Error:', error)
+    alert('An unexpected error occurred.')
   }
 }

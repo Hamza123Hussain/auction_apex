d2426734f5c0ec094b44fd77fda215e32eb34e57
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 03:48:22 +0500

    using seller data as prop to differniate between 2 function of the same comp

--- src/components/Product/ProductPage.tsx
+++ src/components/Product/ProductPage.tsx
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { fetchAllProducts } from '../../functions/Product/GetAllProducts'
 import { ProductCardData } from '../../utils/ProductInterface'
 import ProductCard from './ProductCard'
+import { GetUserProducts } from '../../functions/Product/GettingUserProduct'
 
-const ProductPage = () => {
+const ProductPage = ({ sellerID }: { sellerID: string }) => {
   const [Products, SetProducts] = useState<ProductCardData[]>([])
   useEffect(() => {
     const getProducts = async () => {
-      const Data = await fetchAllProducts()
-      console.log('All Proudcts', Data)
-      SetProducts(Data)
+      const Data = sellerID
+        ? await GetUserProducts(sellerID)
+        : await fetchAllProducts()
+      if (Data) SetProducts(Data)
     }
     getProducts()
   }, [])
 
   return (
-    <div className=" grid grid-cols-1 justify-center items-center gap-5  mx-auto sm:grid-cols-3">
+    <div className=" grid grid-cols-1 justify-center items-center gap-5  mx-auto sm:grid-cols-2">
       {Products.map((element) => (
         <ProductCard productdata={element} key={element._id} />
       ))}

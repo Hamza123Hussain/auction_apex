f05e81cbab7765c5050f95ff117e05899dc19d20
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 03:19:55 +0500

    FUNCTION TO SEND SELLER ID AND GET USER PRODUCTS

---
+++ src/functions/Product/GettingUserProduct.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { APIURL } from '../../utils/SignupInterface'
+
+export const GetUserProducts = async (sellerID: string) => {
+  const Response = await axios.get(
+    `${APIURL}/API/Product/GetUserProducts?sellerID=${sellerID}`
+  )
+  try {
+    if (Response.status === 200) {
+      console.log('product data ', Response.data)
+    }
+  } catch (error) {
+    console.error('Error fetching products:', error)
+    alert('An error occurred while fetching the products.')
+    return []
+  }
+}

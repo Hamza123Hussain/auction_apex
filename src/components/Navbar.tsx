e255e43385a8d1ce16500ac905d61bfb06826f5f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 01:27:32 +0500

    fixed padding issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,36 @@
 import React, { useContext, useState } from 'react'
 import { Activity, Annoyed, Lock, LockOpen } from 'lucide-react'
 import { UserContext } from '../utils/Context'
-import ShowUser from './ShowUser'
+import ShowUser from './Auth/ShowUser'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
   const { userData } = useContext(UserContext)
   return (
-    <nav className="bg-electricBlue text-softWhite py-4 px-6 flex flex-col md:flex-row md:justify-between items-center shadow-lg">
+    <nav className="bg-electricBlue text-softWhite px-4 flex flex-col md:flex-row md:justify-between items-center shadow-lg">
       <div className="flex justify-between w-full md:w-auto">
-        <div className="text-xl font-bold">AuctionApex</div>
-        <button
+        <div className="text-xl font-bold flex items-center ">
+          <img src={'/LOGO.png'} alt="Logo" className=" w-20" />
+          <span>AuctionApex</span>
+        </div>
+        {/* <button
           className="md:hidden flex items-center"
           onClick={() => setIsOpen(!isOpen)}
         >
           {!isOpen ? <Lock /> : <LockOpen />}
-        </button>
+        </button> */}
       </div>
-      <ul
+      {/* <ul
         className={`flex flex-col md:flex-row md:space-x-6 mt-4 md:mt-0 ${
           isOpen ? 'block' : 'hidden'
         }`}
       >
         <li>
           <a href="#" className="hover:text-neonGreen">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-neonGreen">
             Auctions
           </a>
         </li>
-        <li>
-          <a href="#" className="hover:text-neonGreen">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-neonGreen">
-            Contact
-          </a>
-        </li>
-      </ul>
+      </ul> */}
       <div className="mt-4 md:mt-0">
         {!userData._id ? (
           <a

f2f25ce86b562cff644cfe92784df2d227f94d21
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-04 06:39:43 +0500

    adding the submit function

--- src/components/Product/CreateProduct.tsx
+++ src/components/Product/CreateProduct.tsx
@@ -1,15 +1,13 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { UserContext } from '../../utils/Context'
 import ProductFields from './ProductFields'
-
+import { createProduct } from '../../functions/Product/CreateAProduct'
 const ProductAddForm = () => {
-  //   const { productdata, setproductdata } = useContext(UserContext)
-  //   const handleSubmit = async (e: any) => {
-  //     e.preventDefault()
-
-  //     // Add your form submission logic here, e.g., sending data to your backend API
-  //   }
-
+  const { productdata } = useContext(UserContext)
+  const handleSubmit = async () => {
+    const Data = await createProduct(productdata)
+    console.log('API HAS RESPONDED : ', Data)
+  }
   return (
     <div className="flex flex-col bg-gradient-to-t from-[#333333] to-[#1E90FF] p-6 rounded-lg shadow-lg mx-auto w-full max-w-lg sm:max-w-md md:max-w-lg lg:max-w-2xl">
       <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-white mb-6">
@@ -18,7 +16,7 @@ const ProductAddForm = () => {
       <div className="space-y-4">
         <ProductFields />
         <button
-          type="submit"
+          onClick={handleSubmit}
           className="bg-[#39FF14] hover:bg-[#2b9d0b] text-white font-medium py-2 rounded-lg shadow-md transition-all duration-300 mt-4 w-full"
         >
           Add Product
@@ -27,5 +25,4 @@ const ProductAddForm = () => {
     </div>
   )
 }
-
 export default ProductAddForm

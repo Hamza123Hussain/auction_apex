4739c8dcde5330acf34fc880f2009d3975974bd6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 01:28:12 +0500

    showing a dropdown when hovered over username and image

---
+++ src/components/Auth/ShowUser.tsx
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../../utils/Context'
+import { APIURL } from '../../utils/SignupInterface'
+import { useNavigate } from 'react-router-dom'
+import UserDropdown from './UserDropdown'
+
+const ShowUser = () => {
+  const { userData } = useContext(UserContext)
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  return (
+    <div className="relative">
+      <div
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+        className="flex items-center gap-2 cursor-pointer"
+      >
+        <img
+          src={`${APIURL}/${userData.imageurl}`}
+          className="rounded-full w-12"
+          alt={userData.Name}
+        />
+        <h6 className="font-extrabold">{userData.Name}</h6>
+      </div>
+      <UserDropdown
+        dropdownOpen={dropdownOpen}
+        setDropdownOpen={setDropdownOpen}
+      />
+    </div>
+  )
+}
+
+export default ShowUser

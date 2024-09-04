38c018c2d1b113cc84402bdbdef0ece269e38c18
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-04 06:41:54 +0500

    data type of the product card

--- src/utils/ProductInterface.ts
+++ src/utils/ProductInterface.ts
@@ -2,7 +2,17 @@ export interface ProductData {
   productName: string
   description: string
   price: number
-  image: File | null
+  image: File | null | string
+  auctionEndDate: string
+  status: string
+}
+export interface ProductCardData {
+  _id: string
+  sellerId: string
+  productName: string
+  description: string
+  price: number
+  image: string
   auctionEndDate: string
   status: string
 }

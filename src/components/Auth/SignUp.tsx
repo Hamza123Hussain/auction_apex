6404e3c6b791245fc1fa77cfffd9b154c5ded5d2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-06 02:19:13 +0500

    added the uselocation as it was being used in various componets and each components showed diff bits

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react'
-import SignUpTextFields from './SignupFields'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { UserContext } from '../../utils/Context'
 import { RegisterUser } from '../../functions/Auth/Register'
+import TextFields from './SignupFields'
 const SignUp = () => {
   const Router = useNavigate()
   const context = useContext(UserContext)
@@ -20,30 +20,34 @@ const SignUp = () => {
     })
     setLoading(false)
   }
+  const location = useLocation() // get the current location
   // if (loading) return <Loader />
   return (
     <div className="flex flex-col bg-gradient-to-t from-[#333333] to-[#1E90FF] p-6 rounded-lg shadow-lg mx-auto w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl">
       <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-white mb-6">
-        Sign Up
+        {location.pathname === '/Profile' ? 'Update Profile' : 'Sign Up'}
       </h2>
-      <SignUpTextFields />
+      <TextFields />
       <button
         onClick={HandleSignup}
         className="bg-[#39FF14] hover:bg-[#2b9d0b] text-white font-medium py-2 rounded-lg shadow-md transition-all duration-300 mt-4 text-xs sm:text-sm md:text-base w-full max-w-xs mx-auto"
       >
-        Sign Up
+        {location.pathname === '/Profile' ? 'Update Profile' : 'Sign Up'}
       </button>
-      <h6 className="text-xs sm:text-sm md:text-base text-center text-gray-400 mt-4">
-        Already Have An Account? Click Here To{' '}
-        <span
-          onClick={() => Router('/Login')}
-          className="underline cursor-pointer text-[#FF6F61] hover:text-[#FF3B30] transition-all duration-300"
-        >
-          Log In
-        </span>
-      </h6>
+      {location.pathname === '/Profile' ? (
+        ''
+      ) : (
+        <h6 className="text-xs sm:text-sm md:text-base text-center text-gray-400 mt-4">
+          Already Have An Account? Click Here To{' '}
+          <span
+            onClick={() => Router('/Login')}
+            className="underline cursor-pointer text-[#FF6F61] hover:text-[#FF3B30] transition-all duration-300"
+          >
+            Log In
+          </span>
+        </h6>
+      )}
     </div>
   )
 }
-
 export default SignUp

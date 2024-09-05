8b75f9671bb2de97765dd80ff323986c656f22ea
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 07:06:04 +0500

    giving data type

--- src/components/Auction/AllAuctions.tsx
+++ src/components/Auction/AllAuctions.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import { AllAuctions } from '../../functions/Auction/GettingAllAuctions'
 import AuctionCard from './AuctionCard'
+import { AuctionCardData } from '../../utils/AuctionInteface'
 const AuctionsList = () => {
-  const [auctions, setAuctions] = useState<any[]>([])
+  const [auctions, setAuctions] = useState<AuctionCardData[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState('')
   useEffect(() => {
@@ -16,18 +17,14 @@ const AuctionsList = () => {
         setLoading(false)
       }
     }
-
     fetchAuctions()
   }, [])
-
   if (loading) {
     return <div className="text-center text-electricBlue">Loading...</div>
   }
-
   if (error) {
     return <div className="text-center text-brightRed">{error}</div>
   }
-
   return (
     <div className="p-6 max-w-4xl mx-auto  rounded-lg shadow-lg">
       <h1 className="text-3xl font-bold text-brightRed mb-6">All Auctions</h1>
@@ -43,5 +40,4 @@ const AuctionsList = () => {
     </div>
   )
 }
-
 export default AuctionsList

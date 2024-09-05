c097dcfb78f1227e6295f1471f51f3f12e8cca1c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 07:07:28 +0500

    remmoved unsued data and made it responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,31 @@
-import React, { useContext, useState } from 'react'
-import { Activity, Annoyed, Lock, LockOpen } from 'lucide-react'
+import React, { useContext } from 'react'
 import { UserContext } from '../utils/Context'
 import ShowUser from './User/ShowUser'
-
+import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const Router = useNavigate()
   const { userData } = useContext(UserContext)
   return (
-    <nav className="bg-electricBlue text-softWhite px-4 flex flex-col md:flex-row md:justify-between items-center shadow-lg">
-      <div className="flex justify-between w-full md:w-auto">
-        <div className="text-xl font-bold flex items-center ">
-          <img src={'/LOGO.png'} alt="Logo" className=" w-20" />
-          <span>AuctionApex</span>
-        </div>
-        {/* <button
-          className="md:hidden flex items-center"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {!isOpen ? <Lock /> : <LockOpen />}
-        </button> */}
-      </div>
-      {/* <ul
-        className={`flex flex-col md:flex-row md:space-x-6 mt-4 md:mt-0 ${
-          isOpen ? 'block' : 'hidden'
-        }`}
+    <div className="flex sm:justify-between items-center flex-col sm:flex-row w-full md:w-auto bg-electricBlue text-softWhite px-4  shadow-lg">
+      <div
+        onClick={() => Router('/')}
+        className="text-xl font-bold cursor-pointer flex items-center "
       >
-        <li>
-          <a href="#" className="hover:text-neonGreen">
-            Auctions
-          </a>
-        </li>
-      </ul> */}
-      <div className="mt-4 md:mt-0">
-        {!userData._id ? (
-          <a
-            href="/Login"
-            className="bg-brightCoral text-softWhite px-4 py-2 rounded hover:bg-brightRed"
-          >
-            Sign In
-          </a>
-        ) : (
-          <ShowUser />
-        )}
+        <img src={'/LOGO.png'} alt="Logo" className=" w-20" />
+        <span>AuctionApex</span>
       </div>
-    </nav>
+
+      {!userData._id ? (
+        <a
+          href="/Login"
+          className="bg-brightCoral text-softWhite  rounded hover:bg-brightRed"
+        >
+          Sign In
+        </a>
+      ) : (
+        <ShowUser />
+      )}
+    </div>
   )
 }
-
 export default Navbar

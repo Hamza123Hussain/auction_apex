2bd297f9bb9bf34ee8b80417dd2d6a6617ae6d97
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 05:03:18 +0500

    api getting all auction data

---
+++ src/functions/Auction/GettingAllAuctions.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import { APIURL } from '../../utils/SignupInterface'
+
+export const AllAuctions = async () => {
+  const response = await axios.get(`${APIURL}/API/Auction/AllAuctions`)
+  try {
+    if (response.status === 200) {
+      return response.data
+    }
+  } catch (error) {
+    console.error('Error:', error)
+    alert('An unexpected error occurred.')
+  }
+}

2d8268fe4f090996dcf05c469d7badad0713926e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-06 03:40:32 +0500

    not needed

--- src/components/User/TabsArray.ts
+++ src/components/User/TabsArray.ts
@@ -1,6 +1,6 @@
 export const tabs = [
   { name: 'Added Product', value: 'AddedProduct', path: '' },
-  { name: 'Create Auction', value: 'CreateAuction', path: 'create-auction' },
+  //   { name: 'Create Auction', value: 'CreateAuction', path: 'create-auction' },
   { name: 'User Products', value: 'user-products', path: 'user-products' },
   { name: 'User Auctions', value: 'user-auctions', path: 'user-auctions' },
 ]

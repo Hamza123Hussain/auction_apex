825481f7ca0d956cca000a3892bf43bb442e87cf
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-07 00:53:43 +0500

    made footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,25 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-darkCharcoal text-softWhite py-4 px-6 mt-auto">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-sm">
-          <p>
-            &copy; {new Date().getFullYear()} AuctionApex. All rights reserved.
-          </p>
-        </div>
-        <div className="space-x-4 text-sm">
-          <a href="#" className="hover:text-neonGreen">
-            Privacy Policy
-          </a>
-          <a href="#" className="hover:text-neonGreen">
-            Terms of Service
-          </a>
-          <a href="#" className="hover:text-neonGreen">
-            Contact Us
-          </a>
-        </div>
-      </div>
+    <footer className="bg-darkCharcoal text-xs text-center text-softWhite py-4 px-6 mt-auto">
+      <p>&copy; {new Date().getFullYear()} AuctionApex. All rights reserved.</p>
     </footer>
   )
 }

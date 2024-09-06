1821da117c1718ef3ef6ba99cf76bca76774c1d1
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-07 03:01:22 +0500

    socket gets auction data

--- src/components/Auction/AllAuctions.tsx
+++ src/components/Auction/AllAuctions.tsx
@@ -1,30 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { AllAuctions } from '../../functions/Auction/GettingAllAuctions'
 import AuctionCard from './AuctionCard'
 import { AuctionCardData } from '../../utils/AuctionInteface'
+import { io } from 'socket.io-client'
+import { UserContext } from '../../utils/Context'
 const AuctionsList = () => {
+  const { userData } = useContext(UserContext)
   const [auctions, setAuctions] = useState<AuctionCardData[]>([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState('')
+  // const [loading, setLoading] = useState(true)
+  // const [error, setError] = useState('')
   useEffect(() => {
-    const fetchAuctions = async () => {
-      try {
-        const Data = await AllAuctions()
-        setAuctions(Data)
-        setLoading(false)
-      } catch (error) {
-        setError('Failed to fetch auctions')
-        setLoading(false)
+    const socket = io('http://localhost:5000', {
+      transports: ['websocket', 'polling'], // Explicitly specify transports
+    })
+    socket.on('connect_error', (error) => {
+      console.error('Socket.IO connection error:', error)
+    })
+    socket.on('error', (error) => {
+      console.error('Socket.IO error:', error)
+    })
+    socket.emit('AuctionList')
+    socket.on('AuctionListReceived', (data: any) => {
+      setAuctions(data)
+    })
+    return () => {
+      if (socket) {
+        socket.disconnect()
       }
     }
-    fetchAuctions()
-  }, [])
-  if (loading) {
-    return <div className="text-center text-electricBlue">Loading...</div>
-  }
-  if (error) {
-    return <div className="text-center text-brightRed">{error}</div>
-  }
+  }, [userData._id])
+
+  // if (loading) {
+  //   return <div className="text-center text-electricBlue">Loading...</div>
+  // }
+  // if (error) {
+  //   return <div className="text-center text-brightRed">{error}</div>
+  // }
   return (
     <div className="p-6 max-w-4xl mx-auto  rounded-lg shadow-lg">
       <h1 className="text-3xl font-bold text-brightRed mb-6">All Auctions</h1>

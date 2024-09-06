7f735dfdf5063864edd3839eab65f24a78390ff0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-06 17:55:21 +0500

    sending data to the auction card

--- src/components/Auction/SingleAuction.tsx
+++ src/components/Auction/SingleAuction.tsx
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getSingleAuction } from '../../functions/Auction/GettingAsingleAuction'
+import { AuctionCardData, AuctionCreate } from '../../utils/AuctionInteface'
+import AuctionCard from './AuctionCard'
 
 const SingleAuction = () => {
   const { AuctionID } = useParams()
+  const [auctiondata, setdata] = useState<AuctionCardData>()
   useEffect(() => {
     const GetAuctionData = async () => {
       const Data = await getSingleAuction(AuctionID)
-      console.log('DATA OF SINGLE AUCTION ', Data)
+      // console.log('DATA OF SINGLE AUCTION ', Data)
+      setdata(Data)
     }
     GetAuctionData()
-  }, [])
-  return <div className=" text-white">h11111111</div>
+  }, [AuctionID])
+  return <>{auctiondata && <AuctionCard auction={auctiondata} />}</>
 }
 
 export default SingleAuction

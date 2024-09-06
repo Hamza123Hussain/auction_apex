85a590e8191d4e4fa5b93566773869f288db281a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-06 18:31:56 +0500

    SINGLE AUCTION COMPONENT HAS BEEN MADE 70%

--- src/components/Auction/SingleAuction.tsx
+++ src/components/Auction/SingleAuction.tsx
@@ -1,27 +1,45 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useContext, useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import { getSingleAuction } from '../../functions/Auction/GettingAsingleAuction'
 import { AuctionCardData } from '../../utils/AuctionInteface'
 import AuctionCard from './AuctionCard'
-
+import { UserContext } from '../../utils/Context'
+import BidField from './BidField'
+import GoHome from '../GoHome'
 const SingleAuction = () => {
+  const { inputVal } = useContext(UserContext)
   const { AuctionID } = useParams()
   const [auctiondata, setdata] = useState<AuctionCardData>()
+  const navigate = useNavigate()
   useEffect(() => {
     const GetAuctionData = async () => {
       const Data = await getSingleAuction(AuctionID)
-      // console.log('DATA OF SINGLE AUCTION ', Data)
       setdata(Data)
     }
     GetAuctionData()
   }, [AuctionID])
+  const handleBidSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    // Logic to submit the bid goes here
+    // e.g., await submitBid(AuctionID, bidAmount)
+    // console.log('Bid submitted:', bidAmount)
+  }
   return (
-    <div className=" flex flex-col mx-auto p-3">
-      <div className=" sm:w-1/2 mx-auto">
+    <div className="flex flex-col mx-auto p-3 max-w-3xl">
+      <div className="sm:w-1/2 mx-auto mb-6">
         {auctiondata && <AuctionCard auction={auctiondata} />}
       </div>
+      <form onSubmit={handleBidSubmit} className="space-y-4">
+        <BidField />
+        <button
+          type="submit"
+          className="w-full p-3 bg-electricBlue text-softWhite rounded-md hover:bg-blue-700 transition duration-300"
+        >
+          Place Bid
+        </button>
+      </form>
+      <GoHome />
     </div>
   )
 }
-
 export default SingleAuction

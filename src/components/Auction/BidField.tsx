aee0f01b08185833eb73753eaf12b9c849411529
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-06 18:31:38 +0500

    BID FILED MANAGES THE BID STATE

---
+++ src/components/Auction/BidField.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../../utils/Context'
+import { InputValues } from '../../utils/SignupInterface'
+
+const BidField = () => {
+  const { inputVal, setInputVal } = useContext(UserContext)
+
+  const HandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputVal((prev: InputValues) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  return (
+    <div>
+      <label className="block text-sm font-bold mb-2 text-darkCharcoal">
+        Enter Your Bid
+      </label>
+      <input
+        type="number"
+        value={inputVal.bid}
+        onChange={HandleChange}
+        placeholder="Bid amount"
+        name="bid"
+        className="w-full p-2 border border-gray-300 rounded-md text-darkCharcoal bg-softWhite focus:ring-2 focus:ring-electricBlue"
+        required
+      />
+    </div>
+  )
+}
+
+export default BidField
